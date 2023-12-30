8273dc4d40fcf11944188571590bd80c7dc1a73b
BurakUni <21010310070@ogrenci.bartin.edu.tr>
2023-12-30 19:20:05 +0300

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -238,7 +238,7 @@ export default function Title({ type, id }: TitleProps) {
 
                   {'runtime' in data && <span>{getLength(data.runtime)}</span>}
 
-                  {'seasons' in data && <span>{data.seasons} Seasons</span>}
+                  {'seasons' in data && <span>{data.seasons} Sezonlar</span>}
                 </div>
 
                 <p className="title-description">{data.description}</p>
@@ -260,12 +260,12 @@ export default function Title({ type, id }: TitleProps) {
             {'seasons' in data && (
               <div className="title-section">
                 <div className="title-row">
-                  <h3>Episodes</h3>
+                  <h3>Bölümler</h3>
 
                   <select className="title-select" defaultValue={season} onChange={onSeasonChange}>
                     {Array.from({ length: data.seasons }).map((_, i) => (
                       <option key={i} value={i + 1}>
-                        Season {i + 1}
+                        Sezon {i + 1}
                       </option>
                     ))}
                   </select>
@@ -292,7 +292,7 @@ export default function Title({ type, id }: TitleProps) {
 
             {data.suggested.length > 0 && (
               <div className="title-section">
-                <h3>More Like This</h3>
+                <h3>Benzer İçerik</h3>
 
                 <div className="title-cards">
                   {data.suggested.map((media, i) => {
