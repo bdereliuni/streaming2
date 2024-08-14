77ff34d1d8cd271aafebabacf0a7fd04d5298eae
BurakUni <21010310070@ogrenci.bartin.edu.tr>
2024-08-14 20:24:37 +0300

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -167,6 +167,7 @@ export default function Watch() {
           {type === 'series' && episode < maxEpisodes && <i className="fa-regular fa-forward-step right" onClick={() => nav(`/watch/${id}?s=${season}&e=${episode + 1}&me=${maxEpisodes}`)}></i>}
         </div>
 
+        <h2 className="player-title">{getTitle()}</h2>
 
         <iframe allowFullScreen referrerPolicy="origin" title={getTitle()} src={getSource()}></iframe>
       </div>
